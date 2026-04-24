3d4f1971bd0640b3d16535c66eeadc473a4dd355
Mohamed Adan <mohamed.adan@example.com>
2026-04-24 21:52:08 +0000

    fix: Correct order of buildscript and plugins blocks in project build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
-// Top-level build file
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        // This is where buildscript dependencies would go, if any were explicitly needed here.
+        // For example: classpath("com.android.tools.build:gradle:x.y.z")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
