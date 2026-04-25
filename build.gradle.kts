d6f91b752e2c446ce76bc7b349c6ed39acf7790b
nezumikait-max <nezumikait@gmail.com>
2026-04-24 18:03:15 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ buildscript {
 }
 
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
